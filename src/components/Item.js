8b9b72b74516e98a6cd83e55d007699a4deb6fa2
ussenuk <ukimanuka@gmail.com>
2023-12-09 13:00:39 +0300

    understanding CRUD operations in react

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,45 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem, onDeleteItem }) {
+
+  // Add function to handle button click
+  function handleDeleteClick() {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+    method: "DELETE",
+  })
+    .then((r) => r.json())
+    .then(() => onDeleteItem(item));
+  }
+
+
+  function handleAddToCartClick() {
+   // add fetch request
+  fetch(`http://localhost:4000/items/${item.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      isInCart: !item.isInCart,
+    }),
+  })
+    .then((r) => r.json())
+    .then((updatedItem) => onUpdateItem(updatedItem));
+  }
+
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      {/* add the onClick listener */}
+      <button
+        className={item.isInCart ? "remove" : "add"}
+        onClick={handleAddToCartClick}
+      >
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button className="remove" onClick={handleDeleteClick}>Delete</button>
     </li>
   );
 }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,34 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+// destructure the onAddItem prop
+function ItemForm({ onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  // Add function to handle submissions
+  function handleSubmit(e){
+    e.preventDefault();
+    const itemData = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+    fetch("http://localhost:4000/items", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(itemData),
+  })
+    .then((r) => r.json())
+    // call the onAddItem prop with the newItem
+    .then((newItem) => onAddItem(newItem));
+  }
+
+
   return (
-    <form className="NewItem">
+    // Set up the form to call handleSubmit when the form is submitted
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
